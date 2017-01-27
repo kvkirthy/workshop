cd88f1a4028104d83843126ac555541e47062d5c
Venkata Kotaru <VenCKi@Venkatas-MacBook-Pro.local>
2017-01-28 00:17:06 +0530

    typescript sample added

--- Controlled/Typescript/index.js
+++ Controlled/Typescript/index.js
@@ -1,9 +1,41 @@
-var Employee = (function () {
-    function Employee(givenName) {
-        this.givenName = givenName;
+var Calculator = (function () {
+    function Calculator(num1, num2) {
+        this.num1 = num1;
+        this.num2 = num2;
     }
-    Employee.prototype.getDetails = function () {
-        return this.givenName + ":" + this.empId;
+    Calculator.prototype.sum = function () {
+        return this.num1 + this.num2;
     };
-    return Employee;
+    Calculator.prototype.deduct = function () {
+        return this.num1 - this.num2;
+    };
+    Calculator.prototype.multiply = function () {
+        return this.num1 * this.num2;
+    };
+    Calculator.prototype.divide = function () {
+        return this.num1 / this.num2;
+    };
+    Calculator.calculate = function (op) {
+        var elmNum1 = Number(document.getElementById("num1").value);
+        var elmNum2 = Number(document.getElementById("num2").value);
+        var resultElm = document.getElementById("result");
+        var calc = new Calculator(elmNum1, elmNum2);
+        switch (op) {
+            case "+":
+                resultElm.innerText = calc.sum().toString();
+                break;
+            case "-":
+                resultElm.innerText = calc.deduct().toString();
+                break;
+            case "*":
+                resultElm.innerText = calc.multiply().toString();
+                break;
+            case "/":
+                resultElm.innerText = calc.divide().toString();
+                break;
+            default:
+                console.error("invalid operator");
+        }
+    };
+    return Calculator;
 }());
--- Controlled/Typescript/index.ts
+++ Controlled/Typescript/index.ts
@@ -1,11 +1,54 @@
-class Employee{
-    private empId: string
+class Calculator
+{
+  
+    constructor(private num1:number, private num2:number){
 
-    constructor(private givenName: string){
-
+    
     }
 
-    public getDetails():string{
-         return `${this.givenName}:${this.empId}`;
+    sum():number{
+        return this.num1 + this.num2;
     }
+
+    deduct(): number{
+        return this.num1 - this.num2;
+    }
+
+    multiply(): number{
+        return this.num1 * this.num2;
+    }
+
+    divide(): number{
+        return this.num1 / this.num2;
+    }
+
+    static calculate(op){
+        var elmNum1 = Number((document.getElementById("num1") as HTMLInputElement).value);
+        var elmNum2 = Number((document.getElementById("num2") as HTMLInputElement).value);
+        var resultElm = document.getElementById("result");
+        var calc = new Calculator(elmNum1, elmNum2);
+        
+        switch(op){
+            case "+":
+                resultElm.innerText = calc.sum().toString();
+                break;
+            
+            case "-":
+                resultElm.innerText = calc.deduct().toString();            
+                break;
+
+            case "*":
+                resultElm.innerText = calc.multiply().toString();            
+                break;
+
+            case "/":
+                resultElm.innerText = calc.divide().toString();                
+                break;
+
+            default:
+                console.error("invalid operator");
+
+        }
+    }
+
 }
